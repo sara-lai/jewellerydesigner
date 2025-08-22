a8635b1e1632883c672d1886b7f73b8a1a019378
sara-lai <shihualai@gmail.com>
2025-08-22 13:09:28 +0700

    put top bar in component

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -3,14 +3,14 @@
 'use client'
 
 import { useState } from 'react'
-import { Box, Card, Skeleton, SimpleGrid, Image, Flex, Text, Heading, Button, Spinner } from '@chakra-ui/react'
+import { Box, Card, Flex, Spinner } from '@chakra-ui/react'
 import FeaturesPanel from './FeaturesPanel'
-import Link from 'next/link'
 
 import YourAIPhotos from './YourAIPhotos'
 import Favourites from './Favourites'
 import Deleted from './Deleted'
 import PublicModels from './PublicModels'
+import TopBar from './TopBar'
 
 import '@/app/dashboard/dashboard.css'
 
@@ -20,12 +20,9 @@ const Dashboard = ({ latestModel, allModels }) => {
     const [loadingCards, setLoadingCards] = useState([])
     const [tab, setTab] = useState('all')
 
-    // todo - state/clicking to manage "tabs" with underlines (marketplace kampong lah)
-
     function setNewPhotoUI(numPhotos: number){
         // argument is number of photos being generated/ number of cards to display
 
-        // reusing graveyard placeholders
         // todo - loading spinners & counters
         const PhotoCardSkeleton = () => (
             <Card.Root minH='300px'minW='250px' boxShadow="md">
@@ -50,47 +47,8 @@ const Dashboard = ({ latestModel, allModels }) => {
             </Box>
             <Box flex="1" overflowY="auto" className="content-scroll" mb={20} pr={2}>  
                 <Box position="sticky" top={0} zIndex={10} bg='white'>
-                    <Flex justify="space-between" align="center" gap={4} p={2} borderBottom="1px solid rgba(0,0,0,.1)">
-                        <Link href="/dashboard">
-                        {/* <Box fontSize='1.1rem' pl={4} fontWeight='600'>
-                            JewelleryAI
-                        </Box> */}
-                        </Link>
-                        <Flex gap={4} align='center'>
-                        <Box>
-                            <Text fontWeight='bold' color='pink.600' letterSpacing='-.3px'>
-                            100 Credits
-                            </Text>
-                        </Box>
-                        <Box>
-                            <Button className='btn-colors' h='32px !important'>Upgrade</Button>            
-                        </Box>
-                        </Flex>              
-                    </Flex>                
-                    <Flex justify='space-evenly' pt={3} borderBottom='1px solid rgba(0,0,0,.1)'>
-                        <Box cursor='pointer' className={tab === 'public' ? 'active tab' : 'tab'} onClick={()=> setTab('public')}>
-                            <Text pl={4} pr={4}>
-                                Public Models
-                            </Text>
-                        </Box>
-                        <Box cursor='pointer' className={tab === 'all' ? 'active tab' : 'tab'} onClick={()=> setTab('all')}>
-                            <Text pl={4} pr={4}>
-                                Your AI Photos
-                            </Text>
-                        </Box>            
-                        <Box cursor='pointer' className={tab === 'favourites' ? 'active tab' : 'tab'} onClick={()=> setTab('favourites')}>
-                            <Text pl={4} pr={4}>
-                                Favourites
-                            </Text>
-                        </Box> 
-                        <Box pr={8} cursor='pointer' className={tab === 'deleted' ? 'active tab' : 'tab'} onClick={()=> setTab('deleted')}>
-                            <Text pl={4} pr={4}>
-                                Deleted
-                            </Text>
-                        </Box>                                    
-                    </Flex>
+                    <TopBar />
                 </Box>
-
                 <Box mx="auto">
                     {tab === 'all' && <YourAIPhotos loadingCards={loadingCards} currentModel={currentModel} />}
                     {tab === 'favourites' && <Favourites />}
---
+++ src/components/Dashboard/TopBar.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { Box, Flex, Text, Button } from '@chakra-ui/react'
+import Link from 'next/link'
+
+const TopBar = () => {
+    return (
+        <Flex justify="space-between" align="center" gap={4} p={2} borderBottom="1px solid rgba(0,0,0,.1)">
+            <Link href="/dashboard">
+            {/* <Box fontSize='1.1rem' pl={4} fontWeight='600'>
+                JewelleryAI
+            </Box> */}
+            </Link>
+            <Flex gap={4} align='center'>
+            <Box>
+                <Text fontWeight='bold' color='pink.600' letterSpacing='-.3px'>
+                100 Credits
+                </Text>
+            </Box>
+            <Box>
+                <Button className='btn-colors' h='32px !important'>Upgrade</Button>            
+            </Box>
+            </Flex>              
+        </Flex>  
+    )
+}
+
+export default TopBar
