6875e9dcb79985bfeeb5061ca8aafbdc9196bc61
sara-lai <shihualai@gmail.com>
2025-08-28 03:52:03 +0800

    start on nicer landing page

--- src/components/Landing/LandingPage.tsx
+++ src/components/Landing/LandingPage.tsx
@@ -1,20 +1,46 @@
 import { Heading, Flex, Box, Text } from '@chakra-ui/react'
+import Link from 'next/link'
 import "@/app/(landing)/landing.css"
 import SignUps from "./SignUps"
 
+// import Image from "next/image"
+import { Image } from '@chakra-ui/react'
+
 const LandingPage = () => {
     return (
-        <Box p={10}>
-        <Flex justify='center' gap='10rem'>
-            <Flex direction='column' gap='10px'>
-            <Heading>Jewellery Design AI</Heading>
-            <Text>Create your own personal jewellery design AI model</Text>
-            <Text> Upload images & train in YOUR style</Text>
-            <Text>Take professional-grade AI photos, custom backdrops</Text>
-            <Text>Never use a lightbox again!</Text>
-            </Flex>
-            <SignUps />
-        </Flex>
+        <Box className='landing-wrapper'>
+            <Box className='top-nav'>
+                <Box>
+                    Logo
+                </Box>
+                <Box cursor='pointer'>
+                    <Link href='/signin'>
+                        Sign In
+                    </Link>
+                </Box>
+            </Box>
+            <Box className='landing-hero' p={10}>
+                <Flex justify='center' gap='8rem'>
+                    <Flex direction='column' gap='10px'>
+                        <Heading className='landing-title' mb={2}>
+                            <span>Jewellery AI</span>
+                        </Heading>
+                        <Flex gap={3} align='center'>
+                            <Image src='/images/jicon-2.png'height='34px' width='34px' />                    
+                            <Text className='bullet-point'>Create deeply customised AI models</Text>
+                        </Flex>
+                        <Flex gap={3} align='center'>
+                            <Image src='/images/jicon-1.png' height='28px' width='34px' />
+                            <Text className='bullet-point'>Upload your images & train in your style</Text>
+                        </Flex>
+                        <Flex gap={3} align='center'>
+                            <Image src='/images/jicon-3.png' height='30px' width='34px' />
+                            <Text className='bullet-point'>Take professional-grade product photos</Text>
+                        </Flex>                        
+                    </Flex>
+                    <SignUps />
+                </Flex>
+            </Box>
         </Box>
     )
 }
